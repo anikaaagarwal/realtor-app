b8f2c5e2861935df8ab1f411c6fe14bd076668c2
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-06-06 18:42:49 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
     return ( <
         >
         <
-        h1 className = 'text-2xl' > hello world < /h1> <
+        Router >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Home / > }
+        /> <
+        Route path = "/profile"
+        element = { < Profile / > }
+        /> <
+        Route path = "/offers"
+        element = { < Offers / > }
+        /> <
+        Route path = "/sign-in"
+        element = { < SignIn / > }
+        /> <
+        Route path = "/sign-up"
+        element = { < SignUp / > }
+        /> <
+        Route path = "/forgot-password"
+        element = { < ForgotPassword / > }
+        /> <
+        /Routes> <
+        /Router> <
         />
     );
 }
