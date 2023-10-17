2152c34d90fbc147118c4362e07e339028cfb9e3
Anika Agarwal <anikaagarwal1808@gmail.com>
2023-10-17 22:40:31 +0530

    signin image change

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
 import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { toast } from 'react-toastify';
+import lock from '../assets/svg/keylock.jpg';
 
 export default function Signin() {
   const [showPassword, setShowPassword] = useState(false);
@@ -36,7 +37,7 @@ export default function Signin() {
       <h1 className = "text-3xl text-center mt-6 font-bold">Sign In</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
-          <img src='https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=773&q=80' alt='key'
+          <img src={lock} alt='key'
           className='w-full rounded-2xl' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
